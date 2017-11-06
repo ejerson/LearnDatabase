fda9511e24b727b97e443949751abe278bb09af3
ejerson <ejersonb@gmail.com>
2017-11-06 11:43:05 -0700

    Able to make listView items clickable and go to a different activity.

--- app/src/main/java/edu/cnm/deepdive/eb/learndatabase/DatabaseActivity.java
+++ app/src/main/java/edu/cnm/deepdive/eb/learndatabase/DatabaseActivity.java
@@ -1,10 +1,13 @@
 package edu.cnm.deepdive.eb.learndatabase;
 
+import android.content.Intent;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 import edu.cnm.deepdive.eb.learndatabase.database.ColorCursorAdapter;
 import edu.cnm.deepdive.eb.learndatabase.database.DatabaseHelper;
@@ -23,12 +26,43 @@ public class DatabaseActivity extends AppCompatActivity {
 
     databaseListView = (ListView) findViewById(R.id.database_list);
 
+
+    databaseListView.setOnItemClickListener(new OnItemClickListener() {
+      @Override
+      public void onItemClick(AdapterView<?> adapterView, View view, int i, long l) {
+
+//        Log.v("clicked", (String)((TextView) view).getText());
+      startActivity(new Intent(DatabaseActivity.this, TestActivity.class));
+//        Toast toast = Toast.makeText(DatabaseActivity.this, "Item", Toast.LENGTH_SHORT);
+//            toast.show();
+
+
+      }
+    });
+
     dbHelper = new DatabaseHelper(this);
 
     sqLiteDatabase = dbHelper.getReadableDatabase();
 
     refresh();
+
   }
+
+//  public AlertDialog addDeck(View view) {
+//    Builder builder = new Builder(this);
+//
+//    LayoutInflater inflater = this.getLayoutInflater();
+//
+//    View inflatedView = inflater.inflate(R.layout.dialog_add_content, null);
+//    final EditText contentView = inflatedView.findViewById(R.id.new_content);
+//    String content = contentView.getText().toString();
+//    SQLiteDatabase db = this.dbHelper.getWritableDatabase();
+//    this.dbHelper.insert(db, content); // add color to parameter if I want color back
+//    this.refresh();
+//
+//    return builder.create();
+//  }
+
 
   // query the database
   public void refresh() {
@@ -41,6 +75,8 @@ public class DatabaseActivity extends AppCompatActivity {
 
     databaseListView.setAdapter(cursorAdapter);
   }
+
+
 
   public void showAddDialog(View view) {
     AddContentFragment dialog = new AddContentFragment();
---
+++ app/src/main/java/edu/cnm/deepdive/eb/learndatabase/TestActivity.java
@@ -0,0 +1,14 @@
+package edu.cnm.deepdive.eb.learndatabase;
+
+import android.os.Bundle;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+
+public class TestActivity extends AppCompatActivity {
+
+  @Override
+  protected void onCreate(@Nullable Bundle savedInstanceState) {
+    super.onCreate(savedInstanceState);
+    setContentView(R.layout.test_database);
+  }
+}
